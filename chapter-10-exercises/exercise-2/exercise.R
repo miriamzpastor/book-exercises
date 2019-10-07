9a719167c8c8469968104153ae996d2a3ebd411e
miriamzpastor <miriam.zpastor@alumnos.upm.es>
2019-10-07 17:45:24 +0200

    R

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -3,28 +3,28 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-em <- paste(c("Employee"),1:100)
+employee <- paste(c("Employee"),1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-r <- runif(100, 40000, 50000)
+salaries2017 <- runif(100, 40000, 50000)
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-s <- runif(100, -5000, 10000)
+salaryAdjustment <- runif(100, -5000, 10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-salaries <- data.frame(em,r,s)
+salaries <- data.frame(employee,salaries2017 ,salaryAdjustment)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
-salaries$s2018 <- r + s
+salaries$s2018 <- salaries2017  + salaryAdjustment
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
-salaries$raise <- salaries$s2018 >r 
+salaries$raise <- salaries$s2018 >salaries2017
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -35,14 +35,15 @@ salaries$raise <- salaries$s2018 >r
 
 
 # How many employees got a raise?
-
+nrow(salaries[salaries$got_raise==TRUE,"employee" ])
 
 # What was the dollar value of the highest raise?
-
+highest_raise <- max(salaries$salaryAdjustment)
 
 # What was the "name" of the employee who received the highest raise?
-
-
+salaries[ROWS,COLUMNS]
+salaries[salaries$salaryAdjustment == max(salaries$salaryAdjustment)]
+got_biggest_raise <- salaries[alaries$salaryAdjustment== biggest_paycut, "employee"]
 # What was the largest decrease in salaries between the two years?
 
 
@@ -60,3 +61,4 @@ salaries$raise <- salaries$s2018 >r
 
 # Write a .csv file of your salary data to your working directory
 
+write.csv()
